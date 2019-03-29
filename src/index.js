e2cccc54f690cafd29bdd05359c251c7c28ea3b7
Oleg Lukashev <incognitorus@gmail.com>
2019-03-29 12:42:21 +0400

    temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store.js";
+import { router } from "./router.js";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// render the main component
+ReactDOM.render(
+  <Provider store={store}>
+    {router}
+  </Provider>,
+  document.getElementById('app')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Router, Route, IndexRoute } from "react-router";
+import { history } from "./store.js";
+import App from "./components/App";
+
+// build the router
+const router = (
+  <Router onUpdate={() => window.scrollTo(0, 0)} history={history}>
+    <Route path="/" component={App}>
+      {/*<IndexRoute component={Home}/>*/}
+      {/*<Route path="*" component={NotFound}/>*/}
+    </Route>
+  </Router>
+);
+
+// export
+export { router };
